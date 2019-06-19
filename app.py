04c7341c11a6fbb27813271288a7784babf4971e
Deborah Perez <perezdeboraha@gmail.com>
2019-06-18 21:18:16 -0400

    added a free tier heroku postgreSQL database to use for staging

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python
 import os
 from flask import Flask
-
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 #print(os.environ['APP_SETTINGS'])
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+from models import Result
+
 
 @app.route('/')
 def hello():
@@ -14,6 +19,11 @@ def hello():
 @app.route('/<name>')
 def hello_name(name):
     return "Hello {}!".format(name)
+
+#def index(request):
+    #r = requests.get('http://httpbin.org/status/418')
+    #print(r.text)
+    #return HttpResponse('<pre>' + r.text + '</pre>')
 
 if __name__ == "__main__":
     app.run()
--- config.py
+++ config.py
@@ -6,8 +6,8 @@ class Config(object):
     DEBUG = False
     TESTING = False
     CSRF_ENABLED = True
-    SECRET_KEY = 'this-really-needs-to-be-changed'
-
+    SECRET_KEY = 'YES'
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
 class ProductionConfig(Config):
     DEBUG = False
