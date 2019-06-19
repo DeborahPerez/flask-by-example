507dbafe58a1006bd3f77af5f3428ac9af686570
Deborah Perez <perezdeboraha@gmail.com>
2019-06-18 23:36:16 -0400

    imported requests library to send external HTTP GET requests to grab the specific user-provided URL, imported request object from Flask to handle GET and POST requests within the app

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import os
+import requests
 from flask import Flask, request, render_template
 from flask_sqlalchemy import SQLAlchemy
 
@@ -13,7 +14,18 @@ from models import Result
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    errors = []
+    results = {}
+    if request.method == "POST":
+        # get url that the user has entered
+        try:
+            url = request.form['url']
+            r = requests.get(url)
+            print(r.text)
+        except:
+            errors.append("Unable to get URL. Please make sure it's valid and try again.")
+
+    return render_template('index.html', errors=errors, results=results)
 
 #def index(request):
     #r = requests.get('http://httpbin.org/status/418')
